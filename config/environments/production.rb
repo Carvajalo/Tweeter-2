eeef614bf60306651ce3ed3507e300afda4ea0ae
Orlando <carvajalo@uninorte.edu.co>
2021-04-25 03:15:24 -0500

    Fix url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,6 +129,6 @@ Rails.application.configure do
   authentication:       'plain',
   enable_starttls_auto: true }
 
-  config.action_mailer.default_url_options = { 'https://tueter.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'https://tueter.herokuapp.com/' }
 
 end
