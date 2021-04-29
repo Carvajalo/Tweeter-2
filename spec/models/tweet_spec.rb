1686431be88230f10db39ee90df40b1df5c3f331
Orlando <carvajalo@uninorte.edu.co>
2021-04-29 04:34:20 -0500

    Shoulda implementation

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  describe "Validation" do
-    
+  describe "Validations" do
+    it { should validate_presence_of(:text) }
   end
 end
 
